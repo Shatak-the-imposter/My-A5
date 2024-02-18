18d75ceec5f0d2eaed1e9222e9cdda8477013123
Shatak-the-imposter <helloshatak@gmail.com>
2024-02-18 22:15:27 +0600

    Total price calculatio done

--- script/function.js
+++ script/function.js
@@ -1,6 +1,7 @@
 const allSeat = document.getElementsByClassName("seat")
 let availabaleSeat = 40;
 let selectedSeat = 0;
+let totalPrice = 0;
 for (const seat of allSeat) {
     seat.addEventListener('click', function (event) {
         seat.classList.add('bg-[#1DD100]');
@@ -9,8 +10,6 @@ for (const seat of allSeat) {
         setInnerText('availabale-seat', availabaleSeat);
         selectedSeat = selectedSeat + 1;
         setInnerText('selected-seat-count', selectedSeat);
-        // seatNumber = event.target.innerText
-        // console.log(seatNumber);
 
         const selectedContainer = document.getElementById('selected-booked-seat')
         const li = document.createElement('li');
@@ -19,17 +18,25 @@ for (const seat of allSeat) {
         const seatClass = document.createElement('p');
         seatClass.innerText = 'economy'
         const seatFair = document.createElement('p');
-        seatFair.innerText = 565;
-      
+        seatFair.innerText = 550;
+
 
         li.appendChild(seatName);
         li.appendChild(seatClass);
         li.appendChild(seatFair);
-
         selectedContainer.appendChild(li);
+
+        totalPrice = parseInt(totalPrice) +parseInt(seatFair.innerText);
+        setInnerText('total-price' , totalPrice)
+        return totalPrice;
 
     })
 }
+
+function applyCupon(){
+    
+}
+
 
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
